3f4aafbd5d1bf16fb2316bec854166fa0ba97a5a
Kasey Chang <kschang77@yahoo.com>
2020-05-19 17:45:30 -0700

    Updates

---
+++ precache-manifest.03703426ce9921d6ad993dcfd44d97d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f599c916530e0bd5b77c2df5129fc3",
+    "url": "/portfoliov3/index.html"
+  },
+  {
+    "revision": "5eb3ae07c0ed73195098",
+    "url": "/portfoliov3/static/css/2.41f498e0.chunk.css"
+  },
+  {
+    "revision": "a468e57619fcd922f375",
+    "url": "/portfoliov3/static/css/main.995dd988.chunk.css"
+  },
+  {
+    "revision": "5eb3ae07c0ed73195098",
+    "url": "/portfoliov3/static/js/2.38fe6d4d.chunk.js"
+  },
+  {
+    "revision": "3fbfbb92c49b22a4ee797a0b10ff46f1",
+    "url": "/portfoliov3/static/js/2.38fe6d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a468e57619fcd922f375",
+    "url": "/portfoliov3/static/js/main.13d373e7.chunk.js"
+  },
+  {
+    "revision": "40832fc911b2d1ba5c90",
+    "url": "/portfoliov3/static/js/runtime-main.48054dc7.js"
+  }
+]);
